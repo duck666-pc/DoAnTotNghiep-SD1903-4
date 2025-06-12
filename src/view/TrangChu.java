8ce67eee25dbb6b9fb6ff8344f1f47489bffbe87
duck666-pc <minhduc.59h3@gmail.com>
2025-06-12 14:42:39 +0700

    git add . git commit -m "Initial commit"

--- src/view/TrangChu.java
+++ src/view/TrangChu.java
@@ -29,6 +29,7 @@ public class TrangChu extends javax.swing.JFrame {
         jPanel = new javax.swing.JPanel();
         jbtQLNV = new javax.swing.JButton();
         jbtQLSP = new javax.swing.JButton();
+        jbtThoat = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -38,7 +39,7 @@ public class TrangChu extends javax.swing.JFrame {
         jPanel.setLayout(jPanelLayout);
         jPanelLayout.setHorizontalGroup(
             jPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 816, Short.MAX_VALUE)
+            .addGap(0, 820, Short.MAX_VALUE)
         );
         jPanelLayout.setVerticalGroup(
             jPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -59,16 +60,25 @@ public class TrangChu extends javax.swing.JFrame {
             }
         });
 
+        jbtThoat.setText("Thoát");
+        jbtThoat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtThoatActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jbtQLNV, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jbtQLSP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jbtQLNV, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jbtQLSP, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jbtThoat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
@@ -81,7 +91,9 @@ public class TrangChu extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jbtQLNV)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jbtQLSP)))
+                        .addComponent(jbtQLSP)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jbtThoat)))
                 .addContainerGap())
         );
 
@@ -109,6 +121,10 @@ public class TrangChu extends javax.swing.JFrame {
         jPanel.repaint();
         jPanel.revalidate();
     }//GEN-LAST:event_jbtQLNVActionPerformed
+
+    private void jbtThoatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtThoatActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jbtThoatActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -144,5 +160,6 @@ public class TrangChu extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel;
     private javax.swing.JButton jbtQLNV;
     private javax.swing.JButton jbtQLSP;
+    private javax.swing.JButton jbtThoat;
     // End of variables declaration//GEN-END:variables
 }
